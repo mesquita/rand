ff1da193c4d540236ce1910f7d304726ff1c4f6d
Vinicius Mesquita de Pinho <vinicius.pinho@rio.smt.ufrj.br>
2017-03-21 08:50:32 -0300

    add header

---
+++ header.cpp
@@ -0,0 +1,14 @@
+/* Copyright (C) Vinicius M. de Pinho - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* Written by: Vinicius M. de Pinho <viniciusmesquita@poli.ufrj.br>
+*  
+* October 2016
+*
+* Electronic and Computing Engineering Department (DEL)
+* Polytechnic School (Poli)
+* Federal University of Rio de Janeiro (UFRJ)
+*
+* Course: --- 201x.y
+* Professor: 
+*/
